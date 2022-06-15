ff41c2b511b5dfa62321d6e8fe20286551ee900b
Juan Pelaez <juandavid.pelaez2@gmail.com>
2022-06-14 20:46:04 -0500

    modulo historico de compras

--- product_purchase_history/wizards/purchase_history_wizard.py
+++ product_purchase_history/wizards/purchase_history_wizard.py
@@ -3,17 +3,35 @@ from odoo import api, fields, models
 
 class PurchaseHistoryWizard(models.TransientModel):
     _name = "purchase.history.wizard"
+    _description = "Purchase History"
 
+    @api.model
     def _get_purchase_line(self):
+        """
+        Gets purchase order line record from which the button is clicked
+        Returns:
+            odoo record: purchase.order.line
+        """
         return self.env["purchase.order.line"].browse(self._context.get("active_id"))
 
     @api.model
     def _default_product_id(self):
+        """
+        Gets product record of the purchase order line from which the button is clicked
+        Returns:
+            odoo record: product.product
+        """
         line = self._get_purchase_line()
         return line.product_id
 
     @api.model
-    def _get_purchase_invoice_lines(self):
+    def _get_purchase_invoiced_lines(self):
+        """
+        Gets invoice lines with the same product of the purchase order line from which the button is clicked
+        invoice lines are filtered by state=posted, quantity > 0 and sorted by order date (desc)
+        Returns:
+            odoo recordset: account.move.line
+        """
         purchase_line = self._get_purchase_line()
         product_id = purchase_line.product_id.id
         purchase_lines = self.env["purchase.order.line"].search(
@@ -21,12 +39,18 @@ class PurchaseHistoryWizard(models.TransientModel):
         )
         purchase_history_lines = purchase_lines.invoice_lines.filtered(
             lambda line: line.parent_state == "posted" and line.quantity > 0
-        )
+        ).sorted(lambda line: line.purchase_line_id.date_order, reverse=True)
         return purchase_history_lines
 
     @api.model
     def _default_purchase_history_lines(self):
-        purchase_history_lines = self._get_purchase_invoice_lines()
+        """
+        Builds a list with the purchase history lines values,
+        this list will be used to fill purchase_history_line_ids field
+        Returns:
+            List[tuple(0,0, Dict)]: list of tuples with the purchase history lines values
+        """
+        purchase_history_lines = self._get_purchase_invoiced_lines()
         vals = []
         for line in purchase_history_lines:
             invoice_id = line.move_id
@@ -40,11 +64,9 @@ class PurchaseHistoryWizard(models.TransientModel):
             order_date = order_line_id.date_order
             qty_ordered = order_line_id.product_qty
             qty_received = order_line_id.qty_received
-            qty_billed = line.quantity
+            # qty_billed = line.quantity
             product_uom = line.product_uom_id
-            price_unit = line.price_total / (
-                round(product_uom.factor_inv) * line.quantity
-            )
+            price_unit = line.price_total / line.quantity
             subsidiary = order_id.picking_type_id.warehouse_id.name
 
             vals.append(
@@ -59,7 +81,7 @@ class PurchaseHistoryWizard(models.TransientModel):
                         "order_date": order_date,
                         "qty_ordered": qty_ordered,
                         "qty_received": qty_received,
-                        "qty_billed": qty_billed,
+                        # "qty_billed": qty_billed,
                         "product_uom": product_uom.name,
                         "price_unit": price_unit,
                         "subsidiary": subsidiary,
@@ -68,8 +90,10 @@ class PurchaseHistoryWizard(models.TransientModel):
             )
         return vals
 
-    product_id = fields.Many2one("product.product", string="Product")
-    purchase_lines = fields.One2many(
+    product_id = fields.Many2one(
+        "product.product", string="Product", default=_default_product_id
+    )
+    purchase_history_line_ids = fields.One2many(
         "purchase.history.line.wizard",
         "wiz_id",
         string="Purchase History",
@@ -79,17 +103,17 @@ class PurchaseHistoryWizard(models.TransientModel):
 
 class PurchaseHistoryLineWizard(models.TransientModel):
     _name = "purchase.history.line.wizard"
-    _order = "date desc"
+    _description = "Purchase History Lines"
 
     wiz_id = fields.Many2one("purchase.history.wizard")
     invoice_number = fields.Char("Invoice Number")
-    invoice_date = fields.Datetime("Invoice Date")
+    invoice_date = fields.Date("Invoice Date")
     vendor = fields.Char("Vendor")
     order_number = fields.Char("Purchase Number")
-    order_date = fields.Datetime("Purchase Date")
+    order_date = fields.Date("Purchase Date")
     qty_ordered = fields.Float("Ordered Qty")
     qty_received = fields.Float("Received Qty")
-    qty_billed = fields.Float("Billed Qty", help="Quantity billed in invoice")
+    # qty_billed = fields.Float("Billed Qty", help="Quantity billed in invoice")
     product_uom = fields.Char("UoM")
     price_unit = fields.Float("Net cost")
     subsidiary = fields.Char("Subsidiary")
